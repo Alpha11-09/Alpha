d5509a61a5a26cc3cd5207e842cfaed466fa0a76
Alpha11-09 <axiommighty@gmail.com>
2026-03-30 23:37:35 +0200

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,22 @@
+const settings = {
+  packname: 'Alpha',
+  author: 'Alpha',
+  botName: "Alpha Bot",
+  botOwner: 'Alpha',
+  ownerNumber: '263786641436',
+
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+
+  commandMode: "public",
+
+  maxStoreMessages: 20,
+  storeWriteInterval: 10000,
+
+  description: "Alpha Bot - Advanced WhatsApp automation system ⚡",
+
+  version: "4.0.0",
+
+  updateZipUrl: "https://github.com/Alpha11-09/alpha/archive/refs/heads/main.zip"
+};
+
+module.exports = settings;
